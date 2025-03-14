a086a69f1a6c28442d7235c8ba32a1da6199892c
riwata <45054468+riwata@users.noreply.github.com>
2025-03-14 04:56:48 +0000

    パスワード変更

--- azure-upload-file-to-storage/app/public/auth.js
+++ azure-upload-file-to-storage/app/public/auth.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', async function() {
     const hashBuffer = await crypto.subtle.digest('SHA-256', data);
     const hashArray = Array.from(new Uint8Array(hashBuffer));
     const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-    const correctHash = "2bb80d537b1da3e38bd30361aa855686bde0ba1d5f2bcd9e42c5f65b9f5f3f29"; // "secret" のSHA-256
+    const correctHash = "37b72d911201662a9113cda9a3d2627fb1e2d175567e5eec816a27252b851d1d"; // "secret" のSHA-256
     if (hashHex !== correctHash) {
       alert("パスワードが違います");
       document.body.innerHTML = "";
